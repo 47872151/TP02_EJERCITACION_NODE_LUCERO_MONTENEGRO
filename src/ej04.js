0571ff8de5b1706b8dbe7a04c1e8f5807e14cb63
Android Studio <devandroid@ort>
2025-03-31 08:49:39 -0300

    ej04

--- src/ej04.js
+++ src/ej04.js
@@ -1,20 +1,25 @@
 import fs from 'fs';
 
-
-const archivoEntrada           = "./archivoEntrada.txt";
-const archivoSalida          = "./archivoSalida.txt";
+const archivoEntrada = "./archivoEntrada.txt";
+const archivoSalida = "./archivoSalida.txt";
 
 console.clear();
 
 copiar(archivoEntrada, archivoSalida);
 
-function copiar(origen, destino){
-   
-    fs.readFile('./archivoEntrada.txt', { encoding: 'utf8'}, (err, data)=> {
-        if (err) throw err;
-        console.log(data);
-      });
+function copiar(origen, destino) {
+    fs.readFile(origen, { encoding: 'utf8' }, (err, data) => {
+        if (err) {
+            console.error('Error al leer el archivo de origen:', err);
+            return;
+        }
 
-      
-
+        fs.writeFile(destino, data, (err) => {
+            if (err) {
+                console.error('Error al escribir el archivo de destino:', err);
+                return;
+            }
+            console.log(`Archivo copiado exitosamente de ${origen} a ${destino}`);
+        });
+    });
 }
