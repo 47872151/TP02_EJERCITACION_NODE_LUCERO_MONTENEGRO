2226fd5e73ac107680b01d87bdfc7ff99c6c6d73
Android Studio <devandroid@ort>
2025-04-22 15:39:58 -0300

    ano

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express"; 
 import cors from "cors";
 import {sumar, restar, multiplicar, dividir} from './src/modules/matematica.js'
-const { searchByPage, searchComplete, getByOmdbId } = require('./omdb-wrapper');
 const app = express();
 const port = 3000;
 
@@ -17,17 +16,17 @@ app.get('/saludar/:nombre', (req, res) => {
   res.status(200).send('Hola ' + req.params.nombre);
 })
 
- app.get('/validarfecha', (req, res) => {
-     const { ano, mes, dia } = req.params;
-     const fechaString = `${ano}-${mes}-${dia}`;
-     const fecha = Date.parse(fechaString);
+app.get('/validarfecha/:ano/:mes/:dia', (req, res) => {
+  const { ano, mes, dia } = req.params;
+  const fechaString = `${ano}-${mes}-${dia}`;
+  const fecha = Date.parse(fechaString);
 
-     if (isNaN(fecha)) {
-        res.status(400).send('Fecha inválida');
-     } else {
-        res.status(200).send('Fecha válida');
-     }
-     })
+  if (isNaN(fecha)) {
+    res.status(400).send('Fecha inválida');
+  } else {
+    res.status(200).send('Fecha válida');
+  }
+})
 
 //b
 app.get('/matematica/sumar', (req, res) => {
@@ -61,75 +60,6 @@ app.get('/matematica/dividir', (req, res) => {
   const resultado = dividir(n1, n2);
   res.status(200).send(`Resultado: ${resultado}`);
 });
-
-//c
-app.get('/omdb/searchbypage', async (req, res) => {
-  try {
-    const { search, p } = req.query;
-    const page = p || 1;
-    const resultado = await searchByPage(search, page);
-
-    res.status(200).json({
-      respuesta: true,
-      cantidadTotal: resultado.totalResults || 0,
-      datos: resultado.Search || [],
-    });
-  } catch (error) {
-    res.status(400).json({
-      respuesta: false,
-      cantidadTotal: 0,
-      datos: [],
-    });
-  }
-});
-
-app.get('/omdb/searchcomplete', async (req, res) => {
-  try {
-    const { search } = req.query;
-    const resultado = await searchComplete(search);
-
-    res.status(200).json({
-      respuesta: true,
-      cantidadTotal: resultado.totalResults || 0,
-      datos: resultado.Search || [],
-    });
-  } catch (error) {
-    res.status(400).json({
-      respuesta: false,
-      cantidadTotal: 0,
-      datos: [],
-    });
-  }
-});
-
-app.get('/omdb/getbyomdbid', async (req, res) => {
-  try {
-    const { imdbID } = req.query;
-    const resultado = await getByOmdbId(imdbID);
-
-    res.status(200).json({
-      respuesta: true,
-      cantidadTotal: 1,
-      datos: resultado || {},
-    });
-  } catch (error) {
-    res.status(400).json({
-      respuesta: false,
-      cantidadTotal: 0,
-      datos: {},
-    });
-  }
-});
-
-//d
-
-
-
-
-//e
-
-
-
 
 
 
